4be1c4ad922ffccdd0d3f253232a5cd540d8cd5d
gdmec07131032 <592876487@qq.com>
2014-09-19 11:03:01 +0800

    what the fuck

---
+++ gen/cn/edu/gdmec/gdmec07131038/chaosapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.gdmec.gdmec07131038.chaosapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
